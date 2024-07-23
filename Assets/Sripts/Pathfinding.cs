8789eece7fbe1aed64668b98d5050e0ae0ab99bd
CorruptionDragon <thomasfulmer17@gmail.com>
2024-07-23 16:53:57 -0500

    path

--- Assets/Sripts/Pathfinding.cs
+++ Assets/Sripts/Pathfinding.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Pathfinding : MonoBehaviour
 {
+    [SerializeField]
+    private Transform[] waypoints;
+
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    private int waypointIndex = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = waypoints[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+        }
+
+        if (transform.position == waypoints[waypointIndex].transform.position)
+        {
+            waypointIndex += 1;
+        }
     }
 }
