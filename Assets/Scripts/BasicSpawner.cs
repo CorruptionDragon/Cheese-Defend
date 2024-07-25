2c1c87d54bc130dabd3e9a15c2aa5f9c18bb5860
CorruptionDragon <thomasfulmer17@gmail.com>
2024-07-25 18:16:47 -0500

    spawner

--- Assets/Scripts/BasicSpawner.cs
+++ Assets/Scripts/BasicSpawner.cs
@@ -13,12 +13,13 @@ public class BasicSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(spawnEnemy(roachInterval, cockroachPrefab));
     }
 
     private IEnumerator spawnEnemy(float interval, GameObject enemy)
     {
         yield return new WaitForSeconds(interval);
         GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
     }
 }
