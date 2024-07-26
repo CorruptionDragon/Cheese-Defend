bf745136e803baa288d9823798c450ee118e5435
CorruptionDragon <thomasfulmer17@gmail.com>
2024-07-26 17:28:14 -0500

    Blibkred

--- Assets/Scripts/BasicRoach.cs
+++ Assets/Scripts/BasicRoach.cs
@@ -16,9 +16,18 @@ public class BasicRoach : MonoBehaviour
     {
         health--;
 
+        StartCoroutine(BlinkRed());
+
         if (health <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    IEnumerator BlinkRed()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+        yield return new WaitForSeconds(0.2f);
+        GetComponent<SpriteRenderer>().color = Color.white;
     }
 }
