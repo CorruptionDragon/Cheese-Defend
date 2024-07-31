a0d26da53d7125c2c594881fd6b3385cb3bf8a37
SpookyOokieCat <wiimusic123@gmail.com>
2024-07-31 17:05:17 -0600

    thingy

---
+++ Assets/Scripts/CreditsSceneStart.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CreditsScene : MonoBehaviour
+{
+    public void LoadScene(string Credits_Scene)
+    {
+        SceneManager.LoadScene(Credits_Scene);
+
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/MainMenuStart.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuStart : MonoBehaviour
+{
+    public void LoadScene(string Main_Menu)
+    {
+        SceneManager.LoadScene(Main_Menu);
+
+
+    }
+
+
+
+}
