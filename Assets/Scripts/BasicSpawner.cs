922c9852125e24a61c1f99d423ccce4d7582a95a
CorruptionDragon <thomasfulmer17@gmail.com>
2024-07-31 18:20:58 -0500

    roach

--- Assets/Scripts/BasicSpawner.cs
+++ Assets/Scripts/BasicSpawner.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class BasicSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject cockroachPrefab;
-    [SerializeField] private GameObject sonictheroach;
-    [SerializeField] private GameObject burnthehouseroach;
+    [SerializeField] private GameObject basicroachPrefab;
+    [SerializeField] private GameObject fastroachPrefab;
+    [SerializeField] private GameObject tankroachPrefab;
 
+    [SerializeField] private float basicroachInterval = 3f;
+    [SerializeField] private float fastroachInterval = 3f;
+    [SerializeField] private float tankroachInterval = 3f;
 
+    private void Start()
+    {
+        StartCoroutine(spawnEnemy())
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5f), Random.Range(-6f, 6f), 0), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
+    }
 }
